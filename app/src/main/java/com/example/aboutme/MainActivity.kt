1a94bf7bfe23159ff00482ee0bc087cd8847305f
JessicaG <61400580+Jessica-star97@users.noreply.github.com>
2022-10-11 17:21:58 -0500

    Tercera parte

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,25 +1,25 @@
 package com.example.aboutme
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-//import androidx.databinding.DataBindingUtil
-//import com.example.android.aboutme.databinding.ActivityMainBinding
-
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    //private val myName: MyName = MyName("")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
     }
     private fun addNickname(view:View){
@@ -27,10 +27,12 @@ class MainActivity : AppCompatActivity() {
         val editText= findViewById<EditText>(R.id.nickname_edit)
         val nicknameTextView= findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text= editText.text
-        editText.visibility=View.GONE
-        view.visibility=View.GONE
-        nicknameTextView.visibility=View.VISIBLE
+        binding.apply {
+        binding.nicknameText.text = binding.nicknameEdit.text
+        binding.nicknameEdit.visibility = View.GONE
+        binding.doneButton.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE
+            }
 
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
@@ -41,9 +43,12 @@ class MainActivity : AppCompatActivity() {
     }
     private fun updateNickname(view: View){
         val editText= findViewById<EditText>(R.id.nickname_edit)
-       // //val doneButton= findViewById<Button>(R.id.done_button)
+        val doneButton= findViewById<Button>(R.id.done_button)
 
-        //val doneButton= findViewById<Button>(R.id.binding.doneButton)
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+
 
         //// updateNickname(it)
         editText.requestFocus()
@@ -58,11 +63,4 @@ class MainActivity : AppCompatActivity() {
 
 }
 
-class ActivityMainBinding {
 
-    //val  binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-    //binding.doneButton.setOnClickListener {
-    //    addNickname(it)
-   // }
-
-}
